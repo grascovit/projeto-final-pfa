cdce7d48a993bd770d8fb6105f66616ef9d4216d
pauloXtr3m <paulo-gyn@outlook.com>
2017-12-16 17:00:28 -0200

    Add accounts list to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { Grid } from 'semantic-ui-react'
+import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {Grid, Accordion, Icon, List} from 'semantic-ui-react'
 import Layout from '../components/layout/Layout'
 
 class Home extends Component {
@@ -10,7 +10,47 @@ class Home extends Component {
     super(props)
 
     this.state = {
+      activeIndex: null
     }
+  }
+
+  handleClick = (e, titleProps) => {
+    const {index} = titleProps
+    const {activeIndex} = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({activeIndex: newIndex})
+  }
+
+  renderAccount = (account, index) => {
+    const {activeIndex} = this.state
+    return (
+
+      <div>
+        <Accordion.Title active={activeIndex === index} index={index} onClick={this.handleClick}>
+          <Icon name='dropdown'/>
+          {account.name}
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === index}>
+          {account.transactions.map((transaction, index) => {
+            return this.renderTransaction(transaction, index)
+          })}
+        </Accordion.Content>
+
+      </div>
+
+    )
+  }
+
+  renderTransaction = (transaction, index) => {
+    const {activeIndex} = this.state
+    return (
+      <List.Item active={activeIndex === index}>
+        <List.Content>
+          <List.Header>{transaction.type} no valor de $ {transaction.value}</List.Header>
+        </List.Content>
+      </List.Item>
+    )
   }
 
   render() {
@@ -19,7 +59,12 @@ class Home extends Component {
         <Grid columns={16} stackable padded='vertically'>
           <Grid.Row>
             <Grid.Column width={16} stretched>
-              Home
+              <Accordion>
+                {this.props.accounts.map((account, index) => {
+                  return this.renderAccount(account, index)
+                })}
+
+              </Accordion>
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -29,6 +74,7 @@ class Home extends Component {
 }
 
 const mapStateToProps = state => ({
+  accounts: state.accounts
 })
 
 Home.contextTypes = {
