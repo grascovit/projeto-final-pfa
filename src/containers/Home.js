ad6384f8600d92e719601714fd2163cba5db4987
Gabriel Rascovit <rascovit@gmail.com>
2017-12-16 23:13:18 -0200

    tweaks on account and transaction rendering

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,8 @@ import {connect} from 'react-redux'
 import {Grid, Accordion, Icon, List} from 'semantic-ui-react'
 import Layout from '../components/layout/Layout'
 
+export const TRANSACTION_TYPES = {debit: 'Débito', credit: 'Crédito'}
+
 class Home extends Component {
   constructor(props) {
     super(props)
@@ -15,26 +17,29 @@ class Home extends Component {
   }
 
   handleClick = (e, titleProps) => {
-    const {index} = titleProps
-    const {activeIndex} = this.state
+    const { index } = titleProps
+    const { activeIndex } = this.state
     const newIndex = activeIndex === index ? -1 : index
 
     this.setState({activeIndex: newIndex})
   }
 
   renderAccount = (account, index) => {
-    const {activeIndex} = this.state
-    return (
+    const { activeIndex } = this.state
 
+    return (
       <div>
         <Accordion.Title active={activeIndex === index} index={index} onClick={this.handleClick}>
           <Icon name='dropdown'/>
           {account.name}
         </Accordion.Title>
         <Accordion.Content active={activeIndex === index}>
-          {account.transactions.map((transaction, index) => {
-            return this.renderTransaction(transaction, index)
-          })}
+          <p>Saldo: R${account.balance}</p>
+          {
+            account.transactions.map((transaction, index) => {
+              return this.renderTransaction(transaction, index)
+            })
+          }
         </Accordion.Content>
 
       </div>
@@ -43,11 +48,12 @@ class Home extends Component {
   }
 
   renderTransaction = (transaction, index) => {
-    const {activeIndex} = this.state
+    const { activeIndex } = this.state
+
     return (
       <List.Item active={activeIndex === index}>
         <List.Content>
-          <List.Header>{transaction.type} no valor de $ {transaction.value}</List.Header>
+          <List.Header>{TRANSACTION_TYPES[transaction.type]} no valor de R${transaction.value} no dia {transaction.date}</List.Header>
         </List.Content>
       </List.Item>
     )
@@ -55,15 +61,16 @@ class Home extends Component {
 
   render() {
     return (
-      <Layout router={this.props.history} icon='home' header='Home' subheader='Posição consolidada das carteiras'>
+      <Layout router={this.props.history} icon='home' header='Home' subheader='Posição consolidada das contas'>
         <Grid columns={16} stackable padded='vertically'>
           <Grid.Row>
             <Grid.Column width={16} stretched>
               <Accordion>
-                {this.props.accounts.map((account, index) => {
-                  return this.renderAccount(account, index)
-                })}
-
+                {
+                  this.props.accounts.map((account, index) => {
+                    return this.renderAccount(account, index)
+                  })
+                }
               </Accordion>
             </Grid.Column>
           </Grid.Row>
